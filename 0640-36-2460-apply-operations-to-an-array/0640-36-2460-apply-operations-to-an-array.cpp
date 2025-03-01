8de338e9aeaf75508abbc15e550620f177a86101
S_DAS <158051992+blackcodd@users.noreply.github.com>
2025-03-01 08:59:34 +0600

    Time: 0 ms, Memory: 13.1 MB - LeetHub

---
+++ 0640-36-2460-apply-operations-to-an-array/0640-36-2460-apply-operations-to-an-array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> applyOperations(vector<int>& nums) {
+        int n=nums.size();
+        for(int i=0;i<n-1;i++){
+            if(nums[i]==nums[i+1]) {
+                nums[i]=2*nums[i];
+                nums[i+1]=0;
+               
+            }
+
+        }
+        vector<int>ans;
+        for(auto it:nums){
+            if(it!=0){
+                ans.push_back(it);
+            }
+        }
+        int k=ans.size();
+        for(int i=1;i<=nums.size()-k;i++){
+            ans.push_back(0);
+        }
+        
+        return ans;
+        
+    }
+};
