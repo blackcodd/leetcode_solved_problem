f837686994f59dc30256048e30c50fd101d205db
S_DAS <158051992+blackcodd@users.noreply.github.com>
2025-03-02 09:19:16 +0600

    Time: 0 ms, Memory: 16.3 MB - LeetHub

---
+++ 12874-178-64-minimum-path-sum/12874-178-64-minimum-path-sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int ro=grid.size();
+        int col=grid[0].size();
+
+        vector<vector<int>>dp(ro,vector<int>(col,0));
+        dp[0][0]=grid[0][0];
+        for(int i=1;i<ro;i++){
+            dp[i][0]=dp[i-1][0]+grid[i][0];
+
+        }
+        for(int j=1;j<col;j++){
+            dp[0][j]=dp[0][j-1]+grid[0][j];
+        }
+        for(int i=1;i<ro;i++){
+            for(int j=1;j<col;j++){
+                dp[i][j]=min(dp[i-1][j],dp[i][j-1])+grid[i][j];
+            }
+        }
+        return dp[ro-1][col-1];
+        
+    }
+};
