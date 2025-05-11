8ce3840eea1d19a4283edd29eab7206554c14d97
S_DAS <158051992+blackcodd@users.noreply.github.com>
2025-05-11 09:52:31 +0600

    Time: 0 ms, Memory: 11.9 MB - LeetHub

---
+++ 1192-95-1550-three-consecutive-odds/1192-95-1550-three-consecutive-odds.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        int cnt=0;
+        for(auto it:arr){
+            if(it&1) ++cnt;
+            else cnt=0;
+            if(cnt==3) return true;
+
+        }
+        return false;
+        
+    }
+};
